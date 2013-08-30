68fea53e7ab9b0f711a77eb232ebb49d64d69fb8
rdrakey <ryan@ryandrake.com>
2013-08-30 12:15:26 +1000

    Change home page

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130827011224) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "candidates", force: true do |t|
     t.string   "name"
     t.integer  "party_id"
