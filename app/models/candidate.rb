8252759263def5f12b4ad39b2927758948828f10
hui <hui@hui-AO725.(none)>
2013-09-02 10:05:23 +1000

    elaborated readme

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,17 +1,3 @@
-# == Schema Information
-#
-# Table name: candidates
-#
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  party_id   :integer
-#  winner     :boolean          default(FALSE)
-#  bio        :text
-#  links      :text
-#  created_at :datetime
-#  updated_at :datetime
-#
-
 class Candidate < ActiveRecord::Base
 
 end
