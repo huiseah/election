da74ac981aab46bf15fef95706ef963255af6598
rdrakey <ryan@ryandrake.com>
2013-09-02 13:32:02 +1000

    Change styles

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,6 +3,7 @@ class TweetsController < ApplicationController
     @liberal_tweets = []
     @greens_tweets = []
     @labor_tweets = []
+
     @greens_tweets << Twitter.user_timeline("greens", :count => 10)
     @greens_tweets << Twitter.user_timeline("senatormilne", :count => 10)
     @labor_tweets << Twitter.user_timeline("KRuddMP", :count => 10)
@@ -13,7 +14,8 @@ class TweetsController < ApplicationController
     @greens_tweets.flatten!.sort! { |a, b| b.created_at <=> a.created_at }
     @labor_tweets.flatten!.sort! { |a, b| b.created_at <=> a.created_at }
 
-    @people_tweets = Twitter.search("#kickstarter -rt", :lang => "en").attrs[:statuses]
+
+    @people_tweets = Twitter.search("#ausvotes -rt", :lang => "en", :count => 10).attrs[:statuses]
     expires_in 10.minutes, public: true
   end
 end
