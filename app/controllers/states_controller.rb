74b3cfc299bfe5fe3e13d987f1ba49e306d26e02
rdrakey <ryan@ryandrake.com>
2013-09-02 15:10:34 +1000

    Style map

---
+++ app/controllers/states_controller.rb
@@ -0,0 +1,28 @@
+class StatesController < ApplicationController
+	def qld
+	end
+
+	def nsw
+	end
+
+	def wa
+	end
+
+	def act
+	end
+
+	def vic
+	end
+
+	def nt
+	end
+
+	def tas
+	end
+
+	def sa
+	end
+
+	def can
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Election::Application.routes.draw do
 root :to => 'pages#home'
 get '/about' => 'pages#about', as: "about"
 get '/tweets' => 'tweets#showtweets', as: "tweets"
+get '/qld' => 'states#qld', as: "qld"
+get '/nt' => 'states#nt', as: "nt"
+get '/can' => 'states#can', as: "can"
+get '/wa' => 'states#wa', as: "wa"
+get '/vic' => 'states#vic', as: "vic"
+get '/act' => 'states#act', as: "act"
+get '/tas' => 'states#tas', as: "tas"
+get '/sa' => 'states#sa', as: "sa"
 end
